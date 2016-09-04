c7e1053d502d9f1416dddeb4701a2ad709d3c86f
xingyuji01@163.com <xingyuji01@163.com>
2016-09-05 01:58:39 +0800

    66635435

--- app/src/main/java/com/example/administrator/testcvsgit/MainActivity.java
+++ app/src/main/java/com/example/administrator/testcvsgit/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
 
-    private String name = "222222222222222222";
+    private String name = "5555555555";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
