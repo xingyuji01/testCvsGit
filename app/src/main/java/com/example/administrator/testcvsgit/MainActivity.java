3ed6e01ca8693a3cffe604372c9ed91f158e01d9
xingyuji01@163.com <xingyuji01@163.com>
2016-09-05 02:11:43 +0800

    修改5

--- app/src/main/java/com/example/administrator/testcvsgit/MainActivity.java
+++ app/src/main/java/com/example/administrator/testcvsgit/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
 
-    private String name = "5555555555";
+    private String name = "1111111111111";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
