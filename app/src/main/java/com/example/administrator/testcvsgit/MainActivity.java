6683383524ed882d60455cd322051dfbd70e1257
xingyuji01@163.com <xingyuji01@163.com>
2016-09-06 01:27:30 +0800

    5

--- app/src/main/java/com/example/administrator/testcvsgit/MainActivity.java
+++ app/src/main/java/com/example/administrator/testcvsgit/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
 
-    private String name = "liying";
+    private String name = "beijing";
     private String age = "28";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
