c7d0a0d9f0e7fe9f880c3c469bc448b20d894d42
xingyuji01@163.com <xingyuji01@163.com>
2016-09-06 01:18:49 +0800

    3

--- app/src/androidTest/java/com/example/administrator/testcvsgit/ApplicationTest.java
+++ app/src/androidTest/java/com/example/administrator/testcvsgit/ApplicationTest.java
@@ -7,7 +7,7 @@ import android.test.ApplicationTestCase;
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
-    private String country = "china";
+    private String country = "usa";
     public ApplicationTest() {
         super(Application.class);
     }
