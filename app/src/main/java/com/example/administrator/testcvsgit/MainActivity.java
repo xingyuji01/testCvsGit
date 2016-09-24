8269d8ab66d9c962b35cbc34e5b16eaafd431b12
xingyuji01@163.com <xingyuji01@163.com>
2016-09-24 10:36:45 +0800

    修改

--- app/src/main/java/com/example/administrator/testcvsgit/MainActivity.java
+++ app/src/main/java/com/example/administrator/testcvsgit/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
 
-    private String name = "the modify";
+    private String name = "will go to usa";
     private String age = "28";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
