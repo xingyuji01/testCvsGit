3ca11f0a7407d8c36e1b8e7490a98e8e77da75a4
chen <38033919@qq.com>
2016-09-25 12:45:59 +0800

    18

--- app/src/main/java/com/example/administrator/testcvsgit/MainActivity.java
+++ app/src/main/java/com/example/administrator/testcvsgit/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
 
-    private String name = "will go to USA";
+    private String name = "will go to India";
     private String age = "28";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
